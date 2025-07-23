a39a59ad4ffe6ef27a0e54c24b14d1fc889f7324
NiimiKeita <keita.niimi@nkaihatsu.com>
2025-07-23 22:49:11 +0900

    4-4まで終了！

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 // ControllerクラスがマッピングするURLを定義する
@@ -18,5 +19,14 @@ public class HelloController {
     public String hello1() {
         // Hello Worldという文字列を返す
         return "Hello World";
+    }
+    // GETリクエストを受け取ることを指定する。また、メソッドがマッピングするURLの定義する。
+    @GetMapping("/hello2")
+    // 返される文字列をHTTPレスポンスとすることを示す
+    @ResponseBody
+    // @RequestParam: リクエストパラメーターから受け取ったデータを変数にセット(バインド)する
+    public String hello2(@RequestParam("name") String name) {
+        // リクエストパラメータから取得した名前を用いて、「こんにちは、○○さん」という文字列を返す
+        return "こんにちは、 "  + name + "さん";
     }
 }
